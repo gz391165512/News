bff56d71d29e8aab6b785b5d49f518447f306b88
duanc <duan22334@163.com>
2013-11-01 11:18:26 +0800

    添加了权限先关的model类
    Role 类 ，Function类

---
+++ News/src/main/java/model/user/Role.java
@@ -0,0 +1,29 @@
+package model.user;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 段昶
+ * Date: 13-11-1
+ * Time: 上午11:08
+ * To change this template use File | Settings | File Templates.
+ */
+public class Role {
+    private int roleId;
+    private String  roleName;
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+}
